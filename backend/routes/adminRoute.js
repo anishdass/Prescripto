1992be67460f6b943294c7621c600470a5842ca6
anishdass <anishdassatoffice@gmail.com>
2025-05-12 11:37:51 +0100

    Admin login and token setup

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { addDoctor } from "../controllers/adminController.js";
+import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middleware/multer.js";
+import authAdmin from "../middleware/authAdmin.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/add-doctor", upload.single("imageFile"), addDoctor);
+adminRouter.post(
+  "/add-doctor",
+  authAdmin,
+  upload.single("imageFile"),
+  addDoctor
+);
+adminRouter.post("/login", loginAdmin);
 
 export default adminRouter;
